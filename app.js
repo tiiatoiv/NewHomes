80052e8cb22f6d3faa502deb142520443d175d8c
Misty Bui <mishtybui@gmail.com>
2019-12-02 13:36:01 +0200

    app.js content added

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const app = express();
 const cors = require('cors');
 const port = 5500;
 const dogRoute = require('./routes/dogRoute');
+const userRoute = require('./routes/userRoute');
 app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extedended: true}));
+
+//app.use(express.static('uploads'));
+//app.use('/thumbnails', express.static('thumbnails'));
+
 app.use('/index', dogRoute);
 app.use('/user', userRoute);
 
