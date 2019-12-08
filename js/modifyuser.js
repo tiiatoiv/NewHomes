89850df9c5f2bd696f8f938a1a8284afff6e792d
Tiia Toivonen <tiiatoiv@metropolia.fi>
2019-12-08 12:06:20 +0200

    delete user account and delete post options added, create post changed to a pop up form

--- js/modifyuser.js
+++ js/modifyuser.js
@@ -9,6 +9,7 @@ const modifyUserForm = document.getElementById('modifyUserForm');
 const modifyUserFormbutton = document.getElementById('modifybutton');
 let currentuser;
 
+
 //create options to select the user on the form
 const createUserOptions = (user) => {
     userList.forEach((list) => {
@@ -20,6 +21,7 @@ const createUserOptions = (user) => {
         option.innerHTML = user.username;
         option.classList.add('light-border');
         list.appendChild(option);
+        currentuser = user.id;
     });
 };
 
@@ -35,21 +37,24 @@ const getUsers = async () => {
             const response = await fetch(url + '/user', options);
             const user = await response.json();
             createUserOptions(user);
-            currentuser = user.id;
         } catch (e) {
             console.log(e.message);
         }
         ;
     } catch (e) {
         console.log(e.message);
-    };
+    }
+    ;
 };
 getUsers();
 
-//add event listener to the delete form > try deleting the current user's info from the database
-modifyUserFormbutton.addEventListener('click', async (evt) => {
+//add event listener to the form > post info to the database when submitted
+//and create a new dog
+modifyUserFormbutton.addEventListener('submit', async (evt) => {
+    console.log('wtf');
     evt.preventDefault();
     const fd = new FormData(modifyUserForm);
+    console.log('Got the form.');
     const fetchOptions = {
         method: 'PUT',
         headers: {
@@ -57,32 +62,9 @@ modifyUserFormbutton.addEventListener('click', async (evt) => {
         },
         body: fd,
     };
-    //if user delete success, remove the session token, alert the user and redirect to the index.html
-    //if error when deleting (mostly happens if user has post's, alerts user and redirects to the userpage.html
-        const response = await fetch(url + '/user/' + currentuser, fetchOptions);
-        const json = await response.json();
-        console.log('modify response', json);
-        window.location.replace('userpage.html');
-        window.alert('Your info has been updated.');
-});
-
-
-/**
-modForm.addEventListener('submit', async (evt) => {
-    evt.preventDefault();
-    const data = serializeJson(modForm);
-    const fetchOptions = {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
-        },
-        body: JSON.stringify(data),
-    };
-
-    console.log(fetchOptions);
-    const response = await fetch(url + '/cat', fetchOptions);
+    const response = await fetch(url + '/user/' + currentuser, fetchOptions);
     const json = await response.json();
-    console.log('modify response', json);
-    getCat();
-}); */
+    console.log('add response', json);
+    window.location.replace('userpage.html');
+    // getDog();
+});
